fa5cbf3c4c5c4105e6de089fb2d030163489c016
Georgi Valchev <gvalchev@neterra.net>
2022-01-09 18:45:24 +0200

    Add Book path

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,16 +9,22 @@ class BookController extends Controller
 {
     public function store(BookRequest $request)
     {
-        Book::create($request->validated());
+        $book = Book::create($request->validated());
+
+        return redirect($book->path());
     }
 
     public function update(Book $book, BookRequest $request)
     {
         $book->update($request->validated());
+
+        return redirect($book->path());
     }
 
     public function destroy(Book $book)
     {
         $book->delete();
+
+        return redirect('/books');
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,8 +11,23 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Book query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $title
+ * @property string $author
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
  */
 class Book extends Model
 {
     protected $fillable = ['title', 'author'];
+
+    public function path(): string
+    {
+        return '/books/' . $this->id;
+    }
 }
